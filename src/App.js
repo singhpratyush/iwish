081e9de3df319f38817d4847c067091b71802baa
Pratyush Singh <singh.pratyush96@gmail.com>
2018-05-12 02:40:44 +0530

    Init project

---
+++ src/App.js
@@ -0,0 +1,13 @@
+import React, { Component } from 'react';
+
+class App extends Component {
+  render() {
+    return (
+      <div>
+        Wish for anything you want!
+      </div>
+    );
+  }
+}
+
+export default App;
