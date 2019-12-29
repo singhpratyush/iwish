5204b5da1ed2f953a54e595e3ef1476f96cced91
Pratyush Singh <singh.pratyush96@gmail.com>
2019-12-29 17:28:48 +0530

    Fix shimmer for wish

--- src/components/WishCard/WishLoading.js
+++ src/components/WishCard/WishLoading.js
@@ -14,9 +14,8 @@ export default props => (
 			<rect x="32" y="32" height="18" width="350" />
 			<rect x="32" y="56" height="18" width="100" />
 
-			<rect x="32" y="96" height="40" width="140" />
-			<rect x="402" y="104" height="20" width="92" />
-			<rect x="502" y="96" height="40" width="40" />
+			<rect x="32" rx={4} ry={4} y="96" height="40" width="140" />
+			<rect x="32" y="148" height="24" width="92" />
 		</ContentLoader>
 	</div>
 );
